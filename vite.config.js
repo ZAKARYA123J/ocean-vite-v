60f8ed585aaf99e5386bf4849101a4d2784cd259
ds <zakaryabaouali255@gmail.com>
2024-10-14 16:16:14 +0200

    fd

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/subscriber/', // base path
+  base: '/', // base path
   plugins: [react()],
   resolve: {
     alias: {
